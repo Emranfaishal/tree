12c2530a4eece38006943a5883ded70087d84353
Emranfaishal <emranfaishalrahat@gmail.com>
2026-03-11 22:12:13 +0600

    end add

--- script.js
+++ script.js
@@ -1,192 +1,184 @@
-const categoriesContainer = document.getElementById("categoriesContainer");
-const treesContainer = document.getElementById("treesContainer");
-const loadingSpinner = document.getElementById("loadingSpinner");
-const allTreesbtn = document.getElementById("allTreesbtn");
+const categoriesContainer = document.getElementById("categories-container");
+const treesContainer = document.getElementById("trees-container");
+const loadingSpinner = document.getElementById("loading-spinner");
+const allTreesBtn = document.getElementById("all-trees-btn");
 const treeDetailsModal = document.getElementById("tree-details-modal");
-const modalImage = document.getElementById("modalImage");
-const modalCategory = document.getElementById("modalCategory");
-const modalDescription = document.getElementById("modalDescription");
-const modalPrice = document.getElementById("modalPrice");
-const modalTitle = document.getElementById("modalTitle");
-const cartContainer = document.getElementById("cartContainer");
-const totalPrice = document.getElementById("totalPrice");
-const emptyCartMessage = document.getElementById("emptyCartMessage");
+const modalImage = document.getElementById("modal-image");
+const modalCategory = document.getElementById("modal-category");
+const modalDescription = document.getElementById("modal-description");
+const modalPrice = document.getElementById("modal-price");
+const modalTitle = document.getElementById("modal-title");
+const totalPrice = document.getElementById("total-price");
+const cartContainer = document.getElementById("cart-container");
+const emptyCartContainer = document.getElementById("empty-cart-container");
 let cart = [];
 
-// Loading
+// loadCategories function to display category buttons
+async function loadCategories() {
+    const res = await fetch("https://openapi.programming-hero.com/api/categories");
+    const data = await res.json();
+    data.categories.forEach((category) => {
+        const btn = document.createElement("button");
+        btn.className = "btn btn-outline w-full";
+        btn.textContent = category.category_name;
+        btn.onclick = () => selectCategory(category.id, btn);
+        categoriesContainer.append(btn);
+    });
+};
+loadCategories();
+
+// click category button function to select separately
+async function selectCategory(categoryId, btn) {
+    showLoading();
+
+    const allButtons = document.querySelectorAll("#categories-container button, #all-trees-btn");
+    allButtons.forEach((btn) => {
+        btn.classList.remove("btn-pp")
+        btn.classList.add("btn-outline");
+    });
+    btn.classList.add("btn-pp");
+    btn.classList.remove("btn-outline");
+
+    const res = await fetch(
+        `https://openapi.programming-hero.com/api/category/${categoryId}`,
+    );
+    const data = await res.json();
+    console.log(data);
+    displayTrees(data.plants);
+
+    hideLoading();
+};
+
+// loading spinner funtions
 function showLoading() {
-  loadingSpinner.classList.remove("hidden");
-  treesContainer.innerHTML = "";
+    loadingSpinner.classList.remove("hidden");
+    loadingSpinner.classList.add("flex");
 }
 function hideLoading() {
-  loadingSpinner.classList.add("hidden");
+    loadingSpinner.classList.add("hidden");
 }
 
-// Category btn
-async function loadCategories() {
-  // async await
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/categories",
-  );
-  const data = await res.json();
-  //   console.log(data);
-  //   console.log(categoriesContainer);
-  data.categories.forEach((category) => {
-    // console.log(category);
-    const btn = document.createElement("button");
-    btn.className = "btn btn-outline w-full";
-    btn.textContent = category.category_name;
-    btn.onclick = () => selectCategory(category.id, btn);
-    categoriesContainer.appendChild(btn);
-  });
-}
-
-async function selectCategory(categoryId, btn) {
-  console.log(categoryId, btn);
-  showLoading();
-
-  // Update active button style
-  const allButtons = document.querySelectorAll(
-    "#categoriesContainer button, #allTreesbtn",
-  );
-  //   console.log(allButtons);
-  allButtons.forEach((btn) => {
-    btn.classList.remove("btn-primary");
-    btn.classList.add("btn-outline");
-  });
-
-  btn.classList.add("btn-primary");
-  btn.classList.remove("btn-outline");
-
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/category/${categoryId}`,
-  );
-  const data = await res.json();
-  console.log(data);
-  displayTrees(data.plants);
-
-  hideLoading();
-}
-
-allTreesbtn.addEventListener("click", () => {
-  // Update active button style
-  const allButtons = document.querySelectorAll(
-    "#categoriesContainer button, #allTreesbtn",
-  );
-  //   console.log(allButtons);
-  allButtons.forEach((btn) => {
-    btn.classList.remove("btn-primary");
-    btn.classList.add("btn-outline");
-  });
-
-  allTreesbtn.classList.add("btn-primary");
-  allTreesbtn.classList.remove("btn-outline");
-
-  loadTrees();
-});
-
-// Trees load and display
-async function loadTrees() {
-  showLoading();
-  const res = await fetch("https://openapi.programming-hero.com/api/plants");
-  const data = await res.json();
-  hideLoading();
-  displayTrees(data.plants);
-}
-function displayTrees(trees) {
-  trees.forEach((tree) => {
-    const card = document.createElement("div");
-    card.className = `card bg-white shadow-sm border-b-2 ${tree.price > 500 ? "border-red-500" : "border-green-500"}`;
-    card.innerHTML = `<figure>
-        <img
-          src="${tree.image}"
-          alt="${tree.name}"
-          title="${tree.name}"
-          class="h-48 w-full object-cover cursor-pointer"
-          onclick="openTreeModal(${tree.id})"
-        />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title cursor-pointer hover:text-[#4ade80]" onclick="openTreeModal(${tree.id})">${tree.name}</h2>
-        <p class="line-clamp-2">
-          ${tree.description}
-        </p>
-        <div class="badge badge-success badge-outline">${tree.category}</div>
-
-        <div class="flex justify-between items-center">
-          <h2 class="font-bold text-xl ${tree.price > 500 ? "text-red-500" : "text-[#4ade80]"}">$${tree.price}</h2>
-          <button class="btn btn-primary" onclick="addToCart(${tree.id}, '${tree.name}', ${tree.price})">Cart</button>
-        </div>
-      </div>`;
-    treesContainer.appendChild(card);
-  });
-}
-async function openTreeModal(treeId) {
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/plant/${treeId}`,
-  );
-  const data = await res.json();
-  const plantDetails = data.plants;
-  modalTitle.textContent = plantDetails.name;
-  modalImage.src = plantDetails.image;
-  modalCategory.textContent = plantDetails.category;
-  modalDescription.textContent = plantDetails.description;
-  modalPrice.textContent = plantDetails.price;
-  treeDetailsModal.showModal();
-}
-loadCategories();
-loadTrees();
-
-// cart
-function addToCart(id, name, price) {
-  console.log(id, name, price, "add to cart");
-  const existingItem = cart.find((item) => item.id === id);
-
-  if (existingItem) {
-    existingItem.quantity += 1;
-  } else {
-    cart.push({
-      id,
-      name,
-      price,
-      quantity: 1,
+// all trees btn button
+allTreesBtn.addEventListener("click", () => {
+    const allButtons = document.querySelectorAll("#categories-container button, #all-trees-btn");
+    allButtons.forEach((btn) => {
+        btn.classList.remove("btn-pp")
+        btn.classList.add("btn-outline");
     });
-  }
+    allTreesBtn.classList.add("btn-pp");
+    allTreesBtn.classList.remove("btn-outline");
 
-  updateCart();
+    loadTress();
+})
+
+// displayTrees function to display all plants
+async function loadTress() {
+    showLoading();
+
+    const res = await fetch("https://openapi.programming-hero.com/api/plants");
+    const data = await res.json();
+
+    hideLoading();
+    displayTrees(data.plants);
 }
-function updateCart() {
-  cartContainer.innerHTML = "";
 
-  if (cart.length === 0) {
-    emptyCartMessage.classList.remove("hidden");
-    totalPrice.textContent = `$${0}`;
-    return;
-  }
+function displayTrees(trees) {
+    treesContainer.innerHTML = "";
 
-  emptyCartMessage.classList.add("hidden");
-
-  let total = 0;
-  cart.forEach((item) => {
-    total += item.price * item.quantity;
-    const cartItem = document.createElement("div");
-    cartItem.className = "card card-body bg-slate-100 font-semibold";
-    cartItem.innerHTML = `<div class="flex justify-between items-center">
-                                <div>
-                                    <h2>${item.name}</h2>
-                                    <p> $${item.price} × ${item.quantity}</p>
-                                </div>
-                                <button class="btn btn-ghost" onclick="removeFromCart(${item.id})">X</button>
+    trees.forEach((tree) => {
+        const card = document.createElement("div");
+        card.className = "card bg-white shadow-sm";
+        card.innerHTML = `
+        <figure>
+                            <img src="${tree.image}"
+                                alt="${tree.name}"
+                                class = "h-48 w-full object-cover cursor-pointer"
+                                onclick = "openTreeModal(${tree.id})" />
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title text-sm font-semibold text-[#1F2937] cursor-pointer hover:text-green-700" onclick = "openTreeModal(${tree.id})">${tree.name}</h2>
+                            <p class="text-[12px] text-[#1F2937] line-clamp-2">${tree.description}
+                            </p>
+                            <div class="">
+                                <p class="text-[#15803D] text-sm font-medium bg-[#DCFCE7] p-1 rounded-xl w-[60%] text-center">${tree.category}</p>
                             </div>
-                            <p class="text-right font-semibold text-xl">$${item.price * item.quantity}</p>`;
+                            <div class="flex justify-between items-center">
+                                <h2 class="font-semibold text-xl text-[#4ade80]">$${tree.price}</h2>
+                                <button onclick = "addToCart(${tree.id}, '${tree.name}', '${tree.price}')" class="btn btn-primary">Cart</button>
+                            </div>
+                        </div>
+        `;
+        treesContainer.appendChild(card);
+    });
+};
 
-    cartContainer.appendChild(cartItem);
-  });
-
-  totalPrice.innerText = `$${total}`;
+// cart modal
+async function openTreeModal(treeId) {
+    const res = await fetch(`https://openapi.programming-hero.com/api/plant/${treeId}`);
+    const data = await res.json();
+    const plantDetails = data.plants;
+    modalTitle.textContent = plantDetails.name;
+    modalImage.src = plantDetails.image;
+    modalDescription.textContent = plantDetails.description;
+    modalPrice.textContent = plantDetails.price;
+    modalCategory.textContent = plantDetails.category;
+    treeDetailsModal.showModal();
 }
+loadTress();
+
+// add to cart functionality
+function addToCart(id, name, price) {
+    // console.log(id, name, price);
+    const existingItem = cart.find((item) => item.id === id);
+
+    // price = Number(price);
+
+    if (existingItem) {
+        existingItem.quantity += 1;
+    }
+    else {
+        cart.push({
+            id,
+            name,
+            price,
+            quantity: 1,
+        });
+    }
+    updateCart();
+};
+
+function updateCart() {
+    cartContainer.innerHTML = "";
+
+    if (cart.length === 0) {
+            emptyCartContainer.classList.remove("hidden");
+            totalPrice.textContent = `$${0}`;
+            return;
+        }
+        emptyCartContainer.classList.add("hidden");
+
+    let total = 0;
+    cart.forEach((item) => {
+        total += item.price * item.quantity;
+        const cartItem = document.createElement("div");
+        cartItem.className = "card card-body bg-white";
+        cartItem.innerHTML = `
+        <div class="flex justify-between items-center">
+                                    <div>
+                                        <h2>${item.name}</h2>
+                                        <p>$${item.price} * ${item.quantity}</p>
+                                    </div>
+                                    <button onclick = "removeFromCart(${item.id})" class="btn btn-ghost">X</button>
+                                </div>
+                                <p class="text-right font-semibold text-xl">$${item.price * item.quantity}</p>
+        `;
+        cartContainer.appendChild(cartItem);
+    });
+    totalPrice.innerText = `$${total}`;
+};
+
 function removeFromCart(treeId) {
-  const updatedCartElements = cart.filter((item) => item.id != treeId);
-  cart = updatedCartElements;
-  updateCart();
-}
+    const updateCartElements = cart.filter((item) => item.id != treeId);
+    cart = updateCartElements;
+    updateCart();
+};
